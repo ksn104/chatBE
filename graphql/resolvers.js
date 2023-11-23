a658d950ee5777f3ee28b420a715b6db8fe8cc97
snkwak <snkwak@daimresearch.com>
2023-11-23 17:56:28 +0900

    initial commit

---
+++ graphql/resolvers.js
@@ -0,0 +1,23 @@
+﻿import users from "../database/users.js";
+
+const resolvers = {
+    Query: {
+        users: () => users,
+        user: (_, { id}) => {
+            return books.filter(user=> user.id === id)[0];
+        },
+    },
+    Mutation: {
+        addUser: (_, { ID, password}) =>{
+            if(users.find(user=> user.ID === ID)) return null;
+            const newUser = {
+                id: users.length +1,
+                ID,
+                password,
+            }
+            users.push(newUser)
+            return newUser
+        }
+    }
+}
+export default resolvers
