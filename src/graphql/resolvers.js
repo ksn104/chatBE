dd0dbe58700118eeb21ddc82189b9a6cac4af9bf
snkwak <snkwak@daimresearch.com>
2024-08-09 16:25:38 +0900

    commit

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,23 +1,64 @@
 ﻿import users from "../database/users.js";
+import {PubSub} from "graphql-subscriptions";
+import chats from "../database/chats.js";
+import messages from "../database/messages.js";
 
+
+const pubsub = new PubSub();
 const resolvers = {
     Query: {
         users: () => users,
-        user: (_, { id}) => {
-            return books.filter(user=> user.id === id)[0];
+        user: (_, { index}) => {
+            return users.filter(user=> user.index === index)[0];
         },
+        chats: () => chats,
+        messages: () => messages,
     },
     Mutation: {
         addUser: (_, { ID, password}) =>{
             if(users.find(user=> user.ID === ID)) return null;
             const newUser = {
-                id: users.length +1,
+                index: users.length +1,
                 ID,
                 password,
             }
             users.push(newUser)
             return newUser
+        },
+        addChat: (_, { owner, visitant}) =>{
+            const newChat = {
+                index: chats.length +1,
+                owner,
+                visitant,
+                creatAt: new Date(),
+            }
+            chats.push(newChat)
+            return newChat
+        },
+        addMessage: (_, { 
+            chatIndex,
+            text
+            }) =>{
+            const newMessage = {
+                index: messages.length +1,
+                chatIndex,
+                creatAt: new Date(), 
+                text, 
+                readIt:false, 
+            }
+            messages.push(newMessage)
+            return newMessage
+        },
+    },
+    Subscription: {
+        messageSubscribe: {
+            subscribe: ()=> pubsub.asyncIterator(['POST_CREATED'])
         }
     }
+}
+
+function sendMessage() {
+    pubsub.publish('NUMBER_INCREMENTED', {messageSubscribe: ''}).then(r => {}); 
+    setTimeout(sendMessage, 1000);
 }
 export default resolvers
--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -2,17 +2,58 @@
     type Query {
         users: [User]
         user: User
+        chat: Chat
+        chats: [Chat]
+        message: Message
+        messages: [Message]
     }
     
     type Mutation {
-        addUser(ID: String!, password: String!): User!
+        addUser(
+            ID: String!, 
+            password: String!
+        ): User!
+        
+        addChat(
+            owner: Int!,
+            visitant: Int!
+        ): Chat!
+
+        addMessage(
+            text: String
+        ): Message!
     }
 
+    type Subscription {
+        messageSubscribe: Int
+    }
+
+
     type User {
-        id: Int!
+        index: Int!
         ID: String!
         password: String!
         token: String!
+        email: String
+        mobile: String
+        createAt: String
+        deleteAt: String
+    }
+
+    type Chat {
+        index: Int!
+        owner: Int!
+        visitant: Int!
+        creatAt: String
+        deleteAt: String
+    }
+
+    type Message {
+        index: Int!
+        chatIndex: Int!
+        creatAt: String
+        text: String
+        readAt: String
     }
 `
 
